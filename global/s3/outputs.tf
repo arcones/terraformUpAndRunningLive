28ed2739fa2eea7785b43f407c6c2a31b36b96e4
Marta Arcones <arcones@live.com>
2018-12-08 20:21:26 +0100

    WIP

--- global/s3/outputs.tf
+++ global/s3/outputs.tf
@@ -2,6 +2,6 @@ output "s3_bucket_arn" {
   value = "${aws_s3_bucket.terraform_state.arn}"
 }
 
-output "user_name_arn" {
-  value = "${aws_iam_user.users.*.arn}"
+output "user_names" {
+  value = "${aws_iam_user.users.*.name}"
 }
